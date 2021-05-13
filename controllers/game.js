313925b9dd89eecf7884d409e85bf9b5ae6da627
jtkyber <43868043+jtkyber@users.noreply.github.com>
2021-05-12 22:38:40 -0600

    fix

--- controllers/game.js
+++ controllers/game.js
@@ -2,8 +2,8 @@ const updateWins = (req, res, db) => {
     const { username } = req.body;
     db('users').where('username', '=', username)
     .increment('wins', 1)
-    .then(() => {
-        res.json(true);
+    .then(user => {
+        res.json(user[0].wins);
     })
     .catch(err => res.status(400).json('Could not increment wins'))
 }
