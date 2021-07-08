c3d6eb745ec10664d7bf7e7df720d6e08735fe26
jtkyber <43868043+jtkyber@users.noreply.github.com>
2021-07-08 16:51:51 -0600

    fix

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -1,7 +1,8 @@
 const getTopFive = (req, res, db) => {
+    const userNum = req.query.userNum;
     db('users').orderBy('wins', 'desc')
     .then(user => {
-        res.json(user[0]);
+        res.json(user[userNum]);
     })
     .catch(err => res.status(400).json(err))
 }
