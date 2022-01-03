83eb2c4f84d51fb8c89fef3fc531f907a6489919
jtkyber <43868043+jtkyber@users.noreply.github.com>
2022-01-03 02:23:36 -0500

    fix

--- controllers/friends.js
+++ controllers/friends.js
@@ -8,18 +8,18 @@ const getFriends = (req, res, db) => {
     .catch(err => res.status(400).json('Could not find friends'))
 }
 
-// const findFriend = (req, res, db) => {
-//     const username = req.query.username;
-//     db('users').where('username', '=', username)
-//     .then(user => {
-//         if (user[0].username) {
-//             res.json(user[0])
-//         } else {
-//             throw new Error('Could not find that friend')
-//         }
-//     })
-//     .catch(err => {console.log(err); res.status(400).json(err)})
-// }
+const findFriend = (req, res, db) => {
+    const username = req.query.username;
+    db('users').where('username', '=', username)
+    .then(user => {
+        if (user[0].username) {
+            res.json(user[0])
+        } else {
+            throw new Error('Could not find that friend')
+        }
+    })
+    .catch(err => {console.log(err); res.status(400).json(err)})
+}
 
 const addFriend = (req, res, db) => {
     const { username, friendlist } = req.body;
@@ -92,6 +92,7 @@ const updateOnlineStatus = (req, res, db) => {
 
 module.exports = {
     getFriends,
+    findFriend,
     addFriend,
     addSelfToFriend,
     updateFriendRequests,
