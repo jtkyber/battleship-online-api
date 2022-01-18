79a00683c41dedc94ee655290bb3951f8f8bef66
jtkyber <43868043+jtkyber@users.noreply.github.com>
2022-01-17 23:01:59 -0500

    changed 'getTopFive' endpoint

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -1,10 +1,10 @@
 const getTopFive = (req, res, db) => {
     // const userNum = req.query.userNum;
-    db('users').returning(['username', 'wins'])
+    db('users')
     .orderBy('wins', 'desc')
     .limit(5)
     .then(data => {
-        res.json(data);
+        res.json([data.username, data.wins]);
     })
     .catch(err => res.status(400).json(err))
 }
