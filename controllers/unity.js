dfdd18767e7ee71a7f0ea2f11f89d8918c4d2a24
jtkyber <43868043+jtkyber@users.noreply.github.com>
2022-04-18 20:06:12 -0400

    fix

--- controllers/unity.js
+++ controllers/unity.js
@@ -2,10 +2,10 @@ const postEmail = (req, res, db) => {
     const { email } = req.body;
     db('unity_email_signup').select('email')
     .insert ({
-        email: 'test'
+        email: email
     })
-    .then(user => {
-        res.json(user[0])
+    .then(() => {
+        res.json(true)
     })
     .catch(() => res.status(400).json('unable to post email'))
 }
