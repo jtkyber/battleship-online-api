76e7a4dab468f827af0f08267358fd303245036c
jtkyber <43868043+jtkyber@users.noreply.github.com>
2023-02-10 18:02:39 -0500

    fix

--- controllers/login.js
+++ controllers/login.js
@@ -1,6 +1,6 @@
 const handleLogin = (req, res, db, bcrypt) => {
     const { username, password, socketid } = req.body;
-    res.send(req.body)
+    res.end(req.body)
     db('users').where('username', '=', username)
     .returning('*')
     .then(user => {
