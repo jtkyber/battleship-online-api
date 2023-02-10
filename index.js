5a7fe1900d685de0c02b0a7e9f2ec9ceaa1f82a2
jtkyber <43868043+jtkyber@users.noreply.github.com>
2023-02-10 01:44:25 -0500

    fix

--- index.js
+++ index.js
@@ -96,3 +96,22 @@ app.get('/getEmailCount', (req, res) => { unity.getEmailCount(req, res, db) })
 app.listen(process.env.PORT || 4000, () => {
     console.log(`app is running on port ${process.env.PORT || 4000}`);
 })
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+module.exports = allowCors(app)
