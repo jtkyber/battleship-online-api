45de023471ac0b9e9f636d18a51142db92c43710
jtkyber <43868043+jtkyber@users.noreply.github.com>
2025-02-24 19:46:57 -0500

    Fix

--- index.js
+++ index.js
@@ -30,9 +30,8 @@ const db = knex({
 	client: 'pg',
 	connection: {
 		connectionString: process.env.DATABASE_URL,
-		ssl: {
-			rejectUnauthorized: false,
-		},
+		ssl: { rejectUnauthorized: false },
+		port: 5432,
 	},
 });
 
